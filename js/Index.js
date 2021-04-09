8a2dfa0ef1117fe1e1234fe03dfdc59e323bf3d4
HyperlocalDemosite <tripti.gupta@adglobal360.com>
2021-04-09 12:54:42 +0530

    Heloo

---
+++ js/Index.js
@@ -0,0 +1,12 @@
+{/* <script type="text/javascript">
+$(document).ready(function() {
+  $("#disclaimers").hide();
+  });
+
+$(document).ready(
+    function(){
+        $("#disclaimerbtnid").click(function () {
+            $("#disclaimers").show("slow");
+        });
+ });
+</script> */}
